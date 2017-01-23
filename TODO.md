fdd5045dd0e032762c0fa9a98652d5235e5a3309
Robert Hughes <roberts.ginger.email@gmail.com>
2017-01-23 17:46:04 +0000

    Functional test for multiple users

--- TODO.md
+++ TODO.md
@@ -1,8 +1,5 @@
-Display multiple items in the table
-Support more than one list
-Tidy database after functional test
-Report:
-    And we should now see six tests pass instead of five:
-    Ran 5 tests in 0.010s
-    OK
+Adjust model so that items are associated with different lists
+Add unique URLs for each list
+Add a URL for creating a new list via POST
+Add URLs for adding a new item to an existing list via POST
 
--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -28,7 +28,7 @@ class NewVisitorTest(LiveServerTestCase):
                 staleness_of(old_page)
         )
 
-    def test_can_start_list_and_retrieve_later(self):
+    def test_can_start_list_for_one_user(self):
         # Ellie has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get(self.live_server_url)
@@ -43,7 +43,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'),
                          'Enter a to-do item')
 
-        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # She types "Buy peacock feathers" into a text box (Ellie's hobby
         # is tying fly-fishing lures
         inputbox.send_keys('Buy peacock feathers')
 
@@ -67,13 +67,49 @@ class NewVisitorTest(LiveServerTestCase):
                  '2: Use peacock feathers to make a fly'
             )
 
-        self.fail('Finish the test')
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Ellie starts a new todo list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        with self.wait_for_page_load(timeout=10):
+            self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a uniue URL for her -- there is some
-        # explanatory text to that effect.
+        # She notices that her list has a unique URL
+        ellie_list_url = self.browser.current_url
+        self.assertRegex(ellie_list_url, '/lists/.+')
 
-        # She visits that URL - her to-do list is still there
+        # Now a new user called Frank comes along to the site
+        # Frank uses a browser on a different computer to Ellie
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Frank visits the homepage. There's no sign on Ellie's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Frank starts a new list by entering a new item. He is less
+        # interesting than Ellie.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        with self.wait_for_page_load(timeout=10):
+            self.check_for_row_in_list_table('1: Buy milk')
+
+        # Frank gets his own unique URL
+        frank_list_url = self.browser.current_url
+        self.assertRegex(frank_list_url, '/lists/.+')
+
+        # There's still no trace of Ellie's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep
+
 
 if __name__ == '__main__':
     unittest.main()
