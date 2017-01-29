0aa120941dd0df9fda5c70b22038a32e2738fdd4
Robert Hughes <roberts.ginger.email@gmail.com>
2017-01-29 05:09:59 +0000

    Adjust the new list view for model validation

--- TODO.md
+++ TODO.md
@@ -1,9 +1,4 @@
-Adapt FTs so they can run against a staging server.
-Spin up a server, install required software and point staging/live domains at it.
-Upload code to the server using Git.
-Get a quick & dirty version of the site running on staging domain using Django dev server.
-Manually set up virtualenv on the server (without virtualenvwrapper).
-Move from quick & dirty version to production-ready configuration using Gunicorn, Systemd and domain sockets.
-Write a script to automate deployment of the site.
-Use the script to deploy the production version of the site.
+Write script to automate deployment of new server
+Remove hardcoded URLs from views.py
+Stop blank entries being submitted
 
--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,40 +1,46 @@
 from .base import FunctionalTest
 from selenium.webdriver.common.keys import Keys
 import unittest
+from time import sleep
 
 
 class ItemValidationTest(FunctionalTest):
 
     def test_cannot_add_empty_list_items(self):
         # Ellie goes to the home page and accidentally tries to submit an
-        # empty list item. She hits Enter on the empty inputbox
+        # empty list item. She hits Enter on the empty self.browser.find_element_by_id('id_new_item')
         self.browser.get(self.server_url)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys(Keys.ENTER)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # The homepage refreshes and there is an error message saying that the
         # list items cannot be blank
-        error = self.browser.find_element_by_css_selector('.has-error')
         with self.wait_for_page_load(timeout=10):
-            self.assertEqual(error.text, "You can't have an empty list item")
+            self.assertEqual(
+                    self.browser.find_element_by_class_name('has-error').text,
+                    "You can't have an empty list item",
+                    "First attempt at blank input not returning error."
+            )
 
         # She tries again with some text for the new item which now works
-        inputbox.send_keys('Buy milk')
-        inputbox.send_keys(Keys.ENTER)
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
         with self.wait_for_page_load(timeout=10):
             self.check_for_row_in_list_table('1: Buy milk')
 
         # Perversely she now decides to submit a second blank list item
-        inputbox.send_keys(Keys.ENTER)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # She receives a similar warning on the list page
-        error = self.browser.find_element_by_css_selector('.has-error')
         with self.wait_for_page_load(timeout=10):
-            self.assertEqual(error.text, "You can't have an empty list item")
+            self.assertEqual(
+                    self.browser.find_element_by_class_name('has-error').text,
+                    "You can't have an empty list item",
+                    "Second attempt at blank input not returning error."
+            )
 
         # And she can correct it by filling some text in
-        inputbox.send_keys('Make tea')
-        inputbox.send_keys(Keys.ENTER)
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
         with self.wait_for_page_load(timeout=10):
             self.check_for_row_in_list_table('1: Buy milk')
             self.check_for_row_in_list_table('2: Make tea')
